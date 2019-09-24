78d5e2ce012720814a3b293d5831d9a13fdc322d
Matías Irland <mirland@xmartlabs.com>
2019-09-24 20:47:05 -0300

    Remove kotlin dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,6 @@ plugins {
   jacoco
   `java-gradle-plugin`
   `maven-publish`
-  id("org.jetbrains.kotlin.jvm") version "1.3.50"
   id("com.github.ben-manes.versions") version "0.25.0"
 }
 
@@ -35,7 +34,6 @@ dependencies {
   implementation("com.github.triplet.gradle:play-publisher:2.4.1")
   implementation("com.google.apis:google-api-services-androidpublisher:v3-rev46-1.25.0")
   implementation("io.fabric.tools:gradle:1.31.1")
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.50")
 
   testImplementation("com.android.tools.build:gradle:3.5.0")
   testImplementation("junit:junit:4.12")
@@ -43,7 +41,6 @@ dependencies {
   testImplementation("org.eclipse.jgit:org.eclipse.jgit:3.5.0.201409260305-r")
   testImplementation("org.jetbrains.kotlin:kotlin-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-  testImplementation("org.mockito:mockito-core:3.3.0")
 }
 
 jacoco {
