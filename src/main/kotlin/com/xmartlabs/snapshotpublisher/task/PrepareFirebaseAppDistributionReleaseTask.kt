5334f18cc9f310cb6443461e695ffc1790686311
Matías Irland <matiasirland@gmail.com>
2019-10-07 16:30:15 -0300

    Firebase App distribution integration (#39)

---
+++ src/main/kotlin/com/xmartlabs/snapshotpublisher/task/PrepareFirebaseAppDistributionReleaseTask.kt
@@ -0,0 +1,33 @@
+package com.xmartlabs.snapshotpublisher.task
+
+import com.google.firebase.appdistribution.gradle.UploadDistributionTask
+import com.xmartlabs.snapshotpublisher.Constants
+import com.xmartlabs.snapshotpublisher.utils.snapshotReleaseExtension
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+
+open class PrepareFirebaseAppDistributionReleaseTask : DefaultTask() {
+  @get:Internal
+  lateinit var releaseTask: UploadDistributionTask
+
+  private val generatedReleaseNotesFile by lazy { File(project.buildDir, Constants.OUTPUT_RELEASE_NOTES_FILE_PATH) }
+
+  @Suppress("unused")
+  @TaskAction
+  fun action() {
+    val firebaseConfig = project.snapshotReleaseExtension.firebaseAppDistribution
+    check(firebaseConfig.serviceAccountCredentials?.exists() == true) {
+      "Invalid service credential file."
+    }
+
+    with(releaseTask.appDistributionProperties) {
+      appId = firebaseConfig.appId
+      serviceCredentialsFile = firebaseConfig.serviceAccountCredentials?.absolutePath
+      testers = firebaseConfig.distributionEmails
+      groups = firebaseConfig.distributionGroupAliases
+      releaseNotesFile = generatedReleaseNotesFile.absolutePath
+    }
+  }
+}
