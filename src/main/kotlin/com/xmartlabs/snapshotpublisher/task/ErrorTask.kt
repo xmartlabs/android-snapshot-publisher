13b3d6eacefd4aa95d716dbbceca733ad3d7fb6c
Matías Irland <mirland@xmartlabs.com>
2019-10-15 11:34:39 -0300

    Fix Firebase app distribution credential relative file path issue (#48)

--- src/main/kotlin/com/xmartlabs/snapshotpublisher/task/ErrorTask.kt
+++ src/main/kotlin/com/xmartlabs/snapshotpublisher/task/ErrorTask.kt
@@ -2,7 +2,6 @@ package com.xmartlabs.snapshotpublisher.task
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
-import java.lang.IllegalStateException
 
 open class ErrorTask : DefaultTask() {
   lateinit var message: String
